7696ad4a3728af4a4eb1393a86a03bae7ffba86b
Grant Beard <Grantxbeard@gmail.com>
2022-08-04 16:38:13 -0600

    build API call in display

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './App.css'
 import Display from '../Display/Display'
 import Header from '../Header/Header'
+import Form from '../Form/Form'
 import { Route, Link } from 'react-router-dom'
 
 class App extends Component {
@@ -21,39 +22,32 @@ class App extends Component {
       <main>
       <Header />
       <Route exact path='/' render={() => 
-      <form>
-      <select value={this.state.type} onChange={event => this.getType(event)} name='type' id='type'>
-       <option defaultValue=''>Select a Medium</option>
-       <option value='collage'>Collage</option>
-       <option value='drawing'>Drawing</option>
-       <option value='mixed-media'>Mixed Media</option>
-       <option value='painting'>Painting</option>
-       <option value='sculpture'>Sculpture</option>
-       <option value='all'>All</option>
-      </select>
+      <>
+      <Form getType={this.getType} />
       <Link to={`/${this.state.type}`}>
       <button>GO!</button>
       </Link>
-      </form>
+      </>
+      
       }
       />
       <Route 
-      path='/collage' render={() => <Display />}
+      path='/collage' render={() => <Display type={this.state.type}/>}
       />
       <Route 
-      path='/drawing' render={() => <Display />}
+      path='/drawing' render={() => <Display type={this.state.type}/>}
       />
       <Route 
-      path='/mixed-media' render={() => <Display />}
+      path='/mixed-media' render={() => <Display type={this.state.type}/>}
       />
       <Route 
-      path='/painting' render={() => <Display />}
+      path='/painting' render={() => <Display type={this.state.type}/>}
       />
       <Route 
-      path='/sculpture' render={() => <Display />}
+      path='/sculpture' render={() => <Display type={this.state.type}/>}
       />
       <Route 
-      path='/all' render={() => <Display />}
+      path='/all' render={() => <Display type={this.state.type}/>}
       />
       </main>
     )
--- src/Display/Display.js
+++ src/Display/Display.js
@@ -1,21 +1,71 @@
 import React, { Component } from 'react'
+import Image from '../Image/Image'
 import './Display.css'
 
 class Display extends Component {
-    constructor() {
+    constructor({ type }) {
         super()
         this.state = {
             work: null,
             isLoading: false,
             error: '',
+            type: type,
         }
     }
 
+    componentDidMount = () => {
+        this.getData()
+    }
+    
+    getData = () => {
+        this.setState({ isLoading: true })
+        fetch(`http://localhost:3000/api/v1/work`)
+        .then(res => {
+            if (res.ok) {
+                return res.json()
+            } else {
+                console.log("Error")
+            }
+        })
+        .then(data => {
+            let work = this.getType(data.work)
+            this.setState({
+                work: work,
+                isLoading: false,
+            })
+        })
+        .catch(error => {
+            this.setState({
+                error: error.message
+            })
+            console.log("Error")
+        })
+    }
+
+    getType = (work) => {
+       let medium = work
+        .filter(img => img.type === this.state.type)
+        .map(img => {
+            return (
+                <Image
+                    path={img.img}
+                    id={img.id}
+                    key={img.id}
+                    description={img.description}
+                />
+            )
+       })
+       return medium
+
+    }
+
     render() {
+        console.log(this.state.work)
         return (
             <div className='display'>
             <section className='item-container'>
-
+            {this.state.isLoading && <p className="loading">Loading...</p>}
+            {this.state.error && <h2>{this.state.error}</h2>}
             <h2>DISPLAY</h2>
             </section>
             </div>
