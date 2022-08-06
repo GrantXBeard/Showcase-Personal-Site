daf419468a0b4a6d1ab3c4487a3ce2763f424cf4
Grant Beard <Grantxbeard@gmail.com>
2022-08-06 12:47:51 -0600

    test Bio

--- cypress/e2e/Biospec.cy.js
+++ cypress/e2e/Biospec.cy.js
@@ -1,5 +1,23 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Bio', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3001/')
+    .get('p').click()
+  })
+  it('Should have an image', () => {
+    cy.get('img').should('have.attr', 'src').should('include', 'https://i.ibb.co/Y83Vcjy/IMG-3311.jpg')
+  })
+  it('Should have a biography', () => {
+    cy.get('p').eq(1).contains('Lorem ipsum')
+  })
+  it('Should be able to link to Instagram', () => {
+    cy.get('a').eq(2).should('have.attr', 'href').should('include', 'https://www.instagram.com/grantxbeard/')
+  })
+  it('Should be able to exit back to the main page', () => {
+    cy.get('p').first().click()
+    cy.url().should('eq', 'http://localhost:3001/')
+  })
+  it('Should be able to return home by clicking the title', () => {
+    cy.get('h1').click()
+    cy.url().should('eq', 'http://localhost:3001/')
   })
 })
